7b7c7929ceb46fc7a30a4a5848fee9ccb9efc84a
Perroliga <perroliga@gmail.com>
2026-09-19 09:43:09 +0200

    Update URL construction to use webKey from env

--- runner_horarios.js
+++ runner_horarios.js
@@ -17,7 +17,8 @@ const puppeteer = require('puppeteer');
 
   // Leer la clave desde la variable de entorno de GitHub Actions
   const cronKey = process.env.MI_CRON_KEY;
-  const url = `https://perroliga.wuaze.com/crons/cron_actualizar_fechas.php?key=${cronKey}`;
+  const webKey = process.env.MI_WEB;
+  const url = `${webKey}/crons/cron_actualizar_fechas.php?key=${cronKey}`;
 
   try {
     console.log(`Navegando a: ${url}`);
