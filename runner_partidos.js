8308911d103da206d2b56f3467a1bde800ec55e2
Perroliga <perroliga@gmail.com>
2026-09-19 10:03:16 +0200

    Update URL in runner_partidos.js

--- runner_partidos.js
+++ runner_partidos.js
@@ -18,7 +18,7 @@ const puppeteer = require('puppeteer');
   // Leer la clave desde la variable de entorno de GitHub Actions
   const cronKey = process.env.MI_CRON_KEY;
   const miweb = process.env.MI_WEB;
-  const url = `https://${miweb}/cron_sincronizar_partidos.php?key=${cronKey}`;
+  const url = `https://perroliga.wuaze.com/cron_sincronizar_partidos.php?key=${cronKey}`;
 
   try {
     console.log(`Navegando a: ${url}`);
