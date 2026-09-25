65e5ad152c03cc18579117090c7aa0e644521cf6
Perroliga <perroliga@gmail.com>
2026-09-25 19:25:14 +0200

    Refactor zamora_python.py for API token update

--- zamora_python.py
+++ zamora_python.py
@@ -2,7 +2,7 @@ import requests
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 
-API_TOKEN = "903e69ef2d691d1e470618432fde2c1d2eec0d5f"
+API_TOKEN = "API_BZZOIRO"
 
 LIGA = 3
 TEMPORADA = 1307
